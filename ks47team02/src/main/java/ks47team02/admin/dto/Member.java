a52ae4c0986356b03a7a0ddd6aaa1a44b3170abf
Kingbuk <119140340+Kingbuk@users.noreply.github.com>
2023-07-10 11:55:50 +0900

    [ks47] 임하라 일반과제 html, 자바 패키지와 클래스 설정

--- ks47team02/src/main/java/ks47team02/admin/dto/Member.java
+++ ks47team02/src/main/java/ks47team02/admin/dto/Member.java
@@ -1,9 +1,25 @@
 package ks47team02.admin.dto;
 
 public class Member {
-//
+	private String userId;
+	private String userPw;
+	private String userName;
+	private String userCode;
+	private String levelCode;
+	private String levelName;
+	private String userGender;
+	private String careerCheck;
+	private String gradeFeeCode;
+	private String userGrade;
+	private String freelancerCheck;
+	private String userAddr;
+	private String userEmail;
+	private String userPhone;
+	private String userJoinDate;
+	private String totalScore;
+	private String reportScore;
 	
 
-	//*치킨치치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨치킨
+	
 
 }
