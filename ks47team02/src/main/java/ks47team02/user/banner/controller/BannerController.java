71e48be2a354ed046c3591dcb503a0490869bec5
darda <ifty3@naver.com>
2023-07-11 19:54:43 +0900

    <author> PKM <details> 양식에 맞게 문서 구조 변경

--- ks47team02/src/main/java/ks47team02/user/banner/controller/BannerController.java
+++ ks47team02/src/main/java/ks47team02/user/banner/controller/BannerController.java
@@ -21,7 +21,7 @@ public class BannerController {
 	@GetMapping("/banner/bannerRequest")
 	public String request(Model model) {
 		model.addAttribute("title", "광고 신청");
-		model.addAttribute("titleText", "광고 문의");
+		model.addAttribute("titleText", "광고 신청");
 		model.addAttribute("contents", "광고 단가 및 예상 가격을 확인할 수 있고, 광고 신청 및 조회를 할 수 있습니다.");
 		return "user/banner/bannerRequest";
 	}
@@ -29,7 +29,7 @@ public class BannerController {
 	@GetMapping("/banner/bannerRequestList")
 	public String requestList(Model model) {
 		model.addAttribute("title", "광고 신청 현황");
-		model.addAttribute("titleText", "광고 문의");
+		model.addAttribute("titleText", "광고 신청 현황");
 		model.addAttribute("contents", "광고 단가 및 예상 가격을 확인할 수 있고, 광고 신청 및 조회를 할 수 있습니다.");
 		return "user/banner/bannerRequestList";
 	}
@@ -37,7 +37,7 @@ public class BannerController {
 	@GetMapping("/banner/bannerProcessList")
 	public String processList(Model model) {
 		model.addAttribute("title", "광고 진행 현황");
-		model.addAttribute("titleText", "광고 문의");
+		model.addAttribute("titleText", "광고 진행 현황");
 		model.addAttribute("contents", "광고 단가 및 예상 가격을 확인할 수 있고, 광고 신청 및 조회를 할 수 있습니다.");
 		return "user/banner/bannerProcessList";
 	}
