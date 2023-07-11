73d3271fbe5a2973d2fa329aa51d631a047281e9
darda <ifty3@naver.com>
2023-07-11 19:39:04 +0900

    <author> PKM <details> 양식에 맞게 문서 구조 변경

--- ks47team02/src/main/java/ks47team02/user/banner/mapper/BannerMapper.java
+++ ks47team02/src/main/java/ks47team02/user/banner/mapper/BannerMapper.java
@@ -1,12 +1,12 @@
 package ks47team02.user.banner.mapper;
 
 import ks47team02.user.banner.dto.BannerRequestCompany;
+import ks47team02.user.profile.dto.ProfileAward;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
 @Mapper
 public interface BannerMapper {
-    public List<BannerRequestCompany> getBannerRequestCompany(String bannerRequestCompany);
-
+    public List<BannerRequestCompany> bannerRequestCompanyList();
 }
