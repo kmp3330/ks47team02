4349b045abe6c5443a7efbd1ea4c9f470ea2dcb5
head <head@192.168.10.27>
2023-07-11 10:02:13 +0900

    Merge통한 develop에서 최신 업데이트 하고 싶어서 Commit합니다.

--- ks47team02/src/main/java/ks47team02/user/controller/ProfileController.java
+++ ks47team02/src/main/java/ks47team02/user/controller/ProfileController.java
@@ -12,47 +12,54 @@ public class ProfileController {
 	@GetMapping("/profileIntroList")
 	public String profileIntroList(Model model) {
 		model.addAttribute("title", "메인화면");
-		model.addAttribute("contents", "자기소개 페이지입니다.");
+		model.addAttribute("titleText", "크게 보이는 글씨");
+		model.addAttribute("contents", "작게 보이는 글씨");
 		return "user/profile/profile_intro_list";
 	}
 
 	@GetMapping("/profileSkillList")
 	public String profileSkillList(Model model) {
 		model.addAttribute("title", "메인화면");
-		model.addAttribute("contents", "자기소개 페이지입니다.");
+		model.addAttribute("titleText", "크게 보이는 글씨");
+		model.addAttribute("contents", "작게 보이는 글씨");
 		return "user/profile/profile_skill_list";
 	}
 	
 	@GetMapping("/profileWorkSpecList")
 	public String profileWorkSpecList(Model model) {
 		model.addAttribute("title", "메인화면");
-		model.addAttribute("contents", "자기소개 페이지입니다.");
+		model.addAttribute("titleText", "크게 보이는 글씨");
+		model.addAttribute("contents", "작게 보이는 글씨");
 		return "user/profile/profile_work_spec_list";
 	}
 	@GetMapping("/profileEduSpecList")
 	public String profileEduSpecList(Model model) {
 		model.addAttribute("title", "메인화면");
-		model.addAttribute("contents", "자기소개 페이지입니다.");
+		model.addAttribute("titleText", "크게 보이는 글씨");
+		model.addAttribute("contents", "작게 보이는 글씨");
 		return "user/profile/profile_edu_spec_list";
 	}
 	
 	@GetMapping("/profileCertificateList")
 	public String profileCertificateList(Model model) {
 		model.addAttribute("title", "메인화면");
-		model.addAttribute("contents", "자기소개 페이지입니다.");
+		model.addAttribute("titleText", "크게 보이는 글씨");
+		model.addAttribute("contents", "작게 보이는 글씨");
 		return "user/profile/profile_certificate_list";
 	}
 	
 	@GetMapping("/profileAwardList")
 	public String profileAward(Model model) {
 		model.addAttribute("title", "메인화면");
-		model.addAttribute("contents", "자기소개 페이지입니다.");
+		model.addAttribute("titleText", "크게 보이는 글씨");
+		model.addAttribute("contents", "작게 보이는 글씨");
 		return "user/profile/profile_award_list";
 	}
 	@GetMapping("/profilePortfolioList")
 	public String profilePortfolioList(Model model) {
 		model.addAttribute("title", "메인화면");
-		model.addAttribute("contents", "자기소개 페이지입니다.");
+		model.addAttribute("titleText", "크게 보이는 글씨");
+		model.addAttribute("contents", "작게 보이는 글씨");
 		return "user/profile/profile_portfolio_list";
 			
 
