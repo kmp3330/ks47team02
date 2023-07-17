9edd4bdcab14b0600ac41a4a5f338a663027a56a
limmino <eexx1368@naver.com>
2023-07-17 09:43:17 +0900

    [ks47team02] 임민호 detail : 구인구직 공고 화면연결 및 DB연결 완료

---
+++ ks47team02/src/main/java/ks47team02/user/announcement/dto/Announcement.java
@@ -0,0 +1,30 @@
+package ks47team02.user.announcement.dto;
+
+
+
+import java.sql.Date;
+
+import lombok.Data;
+
+@Data
+public class Announcement {
+
+	private String announcementCode;
+	private String cpId;
+	private String cpName;
+	private String cpCateCode;
+	private String joinCateCode;
+	private String joinCateName;
+	private String areaCode;
+	private String areaCate;
+	private String jobOpening;
+	private String requiredSkills;
+	private String career;
+	private String recruitment;
+	private String wage;
+	private Date startDate;
+	private Date dueDate;
+	private String hiringStatusCode;
+	private String hiringStatus;
+	
+}
