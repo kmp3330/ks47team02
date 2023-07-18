7f735ea49545143f84e2a6cb6b617826ea71b7b4
HOONIkj <clsrn254@gmail.com>
2023-07-18 10:46:40 +0900

    [ks47team02] 김재훈 detail : 로그인 부분 추가 수정

---
+++ ks47team02/src/main/java/ks47team02/common/interceptor/LoginInterceptor.java
@@ -0,0 +1,37 @@
+package ks47team02.common.interceptor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@Component
+public class LoginInterceptor implements HandlerInterceptor {
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		
+		HttpSession session = request.getSession();
+		String sessionId = (String) session.getAttribute("SID");
+		
+		if(sessionId == null) {
+			response.sendRedirect("/login");
+			return false;
+		} 
+		/*
+			 * else { String requestUri = request.getRequestURI(); String sessionLevel =
+			 * (String) session.getAttribute("SLEVEL");
+			 * 
+			 * if(sessionLevel.equals("일반회원")) {
+			 * 
+			 * } }
+			 */
+		
+		
+		return true;
+	}
+	
+}
